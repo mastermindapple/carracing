4dca96b29758ac828e1e6bbfc59bbe47a11c5c33
Janav <dhingrajanavllc@gmail.com>
2020-01-14 19:59:38 +0530

    made changes into sketch.js to get data from database

--- sketch.js
+++ sketch.js
@@ -1,29 +1,47 @@
 var ball;
+var position,database;
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    ballPosition = database.ref("car/position");
+    ballPosition.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+
+    if(position !== undefined){
+        if(keyDown(LEFT_ARROW)){
+            changePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            changePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            changePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            changePosition(0,+1);
+        }
     }
     drawSprites();
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+   database.ref("car/position").set({
+       'x': position.x + x,
+       'y' : position.y + y
+   })
+}
+
+function readPosition(pos){
+    position = pos.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+function showError(){
+    console.log("Cannot Get Data From Database");
 }
